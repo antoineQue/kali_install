2e9555fe34c9f16af22c8b974d6ad3b9658d5c00
antoineQue <antoine.queva@orange.fr>
2019-09-04 18:03:36 +0200

    Update kali_install.sh

--- kali_install.sh
+++ kali_install.sh
@@ -8,7 +8,7 @@ echo "NOTE : THINK TO ACTIVATE BIDIRECTIONAL COPY/PASTE"
 apt update && apt -y full-upgrade 
 
 echo "------- APT INSTALL PACKAGE------" 
-apt install -y virtualbox-guest-x11 apt-transport-https gobuster crackmapexec terminator
+apt install -y virtualbox-guest-x11 apt-transport-https gobuster crackmapexec terminator curl python-pip python3-pip
 
 
 mkdir ~/tools && cd ~/tools 
